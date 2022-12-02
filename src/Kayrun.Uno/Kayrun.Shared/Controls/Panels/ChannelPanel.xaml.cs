50d98c50575fd8530e083b6b0c2d1b3c80f4c022
Adam Dernis <avid29@live.com>
2022-12-02 13:46:57 -0500

    WIP Shell and Channel Panel

---
+++ src/Kayrun.Uno/Kayrun.Shared/Controls/Panels/ChannelPanel.xaml.cs
@@ -0,0 +1,14 @@
+﻿// Adam Dernis 2022
+
+using Microsoft.UI.Xaml.Controls;
+
+namespace Kayrun.Controls.Panels
+{
+    public sealed partial class ChannelPanel : UserControl
+    {
+        public ChannelPanel()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Kayrun.Uno/Kayrun.Shared/Controls/Shell/Shell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Kayrun.Controls.Shell
+{
+    public sealed partial class Shell : UserControl
+    {
+        public Shell()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
